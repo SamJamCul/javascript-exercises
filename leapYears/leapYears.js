e59f6999318432d61254310ccecb4a213802f28d
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-12-20 14:59:10 +0000

    finished leap years

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-var leapYears = function() {
-
+var leapYears = function(year) {
+  if (year % 400 != 0 && year % 100 === 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return false
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(leapYears(1996))
+console.log(leapYears(2000))
+console.log(leapYears(1900))
 
 module.exports = leapYears
