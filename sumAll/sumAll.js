3452c25862c5cdf94d22d6929342b98539e7f171
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-12-20 12:51:49 +0000

    finished sumall

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-var sumAll = function() {
-
+var sumAll = function(x, y) {
+  let large;
+  let small;
+  let total = 0;
+  if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
+    return "ERROR"
+  }
+  if (x < y) {
+    small = x
+    large = y;
+  } else {
+    small = y
+    large = x;
+  }
+  for (var i = small; i <= large; i++) {
+    total += i;
+  }
+  return total;
 }
+
+console.log(sumAll(1, [1, 2]))
 
 module.exports = sumAll
