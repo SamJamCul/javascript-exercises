b76ef5af1ebfed7ed6037c916ecad4ce50d50cbb
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-12-20 12:41:33 +0000

    finished remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-var removeFromArray = function() {
-
+var removeFromArray = function(array, removee) {
+  var arrayLength = array.length;
+  var argLength = arguments.length;
+  for (var i = 0; i < arrayLength; i++) {
+    for (var j = 1; j < argLength; j++) {
+      if (array[i] === arguments[j]) {
+        console.log(array[i])
+        array.splice(i, 1);
+        j--;
+      }
+    }
+  }
+  return array
 }
-
 module.exports = removeFromArray
